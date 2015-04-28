6421c1ff7853ebf4527c06860ebd198d0d53dbd9
kekennedy11 <kekennedy11@gmail.com>
2015-04-28 11:07:21 -0500

    first commit

---
+++ app/scripts/app.js
@@ -0,0 +1,42 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name myYoProjectApp
+ * @description
+ * # myYoProjectApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('myYoProjectApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch',
+    'ui.sortable',
+    'LocalStorageModule'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl'
+      })
+      .when('/pictures', {
+        templateUrl: 'views/pictures.html',
+        controller: 'PicturesCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  })
+  .config(['localStorageServiceProvider', function(localStorageServiceProvider){
+    localStorageServiceProvider.setPrefix('ls');
+  }]);
---
+++ app/scripts/controllers/pictures.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name myYoProjectApp.controller:PicturesCtrl
+ * @description
+ * # PicturesCtrl
+ * Controller of the myYoProjectApp
+ */
+angular.module('myYoProjectApp')
+  .controller('PicturesCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
